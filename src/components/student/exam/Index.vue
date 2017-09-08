8ef84193064aa1a28519066193267c0bb4fe440a
Tomislav-Zoricic <tomislav.zoricic.2@gmail.com>
2017-09-08 13:28:08 +0200

    Added student exam ER diagram template

--- src/components/student/exam/Index.vue
+++ src/components/student/exam/Index.vue
@@ -7,19 +7,21 @@
       </div>
       <div class="column is-one-third">
         <exam-last-actions></exam-last-actions>
+        <exam-erdiagram></exam-erdiagram>
       </div>
     </div>
-    <h1>Active exam</h1>
   </div>
 </template>
 
 <script>
 import Header from './header/Index'
 import LastActions from './lastActions/Index'
+import ERDiagram from './erDiagram'
 export default {
   components: {
     'exam-header': Header,
-    'exam-last-actions': LastActions
+    'exam-last-actions': LastActions,
+    'exam-erdiagram': ERDiagram
   }
 }
 </script>
