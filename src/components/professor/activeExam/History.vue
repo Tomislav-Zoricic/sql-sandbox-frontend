5caa6e088a94dbb29be8f666917989d91361b164
Tomislav-Zoricic <tomislav.zoricic.2@gmail.com>
2017-09-13 13:42:32 +0200

    Added professor active exam history of student answers

--- src/components/professor/activeExam/History.vue
+++ src/components/professor/activeExam/History.vue
@@ -1,9 +1,59 @@
 <template lang="html">
-  <h1>History</h1>
+<div class="">
+  <div class="columns">
+    <div class="column is-half is-offset-one-quarter">
+      <search-control></search-control>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="entry in history" class="box">
+          <div class="media-content">
+            <div class="content">
+              <h4 class="title">
+                <strong>{{entry.student}}</strong>
+                <small>Score</small> <small>{{entry.score}} %</small>
+                <small class="is-pulled-right">{{entry.timestamp}}</small>
+              </h4>
+              <div class="box">
+                <h5 class="subtitle">Question</h5>
+                <p>{{entry.question}}</p>
+              </div>
+              <div class="box">
+                <h5 class="subtitle">Answer</h5>
+                <p>{{entry.answer}}</p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
+import faker from 'faker'
+import SearchControl from './SearchControl'
+
 export default {
+
+  components: {
+    'search-control': SearchControl
+  },
+
+  computed: {
+    history () {
+      return [...Array(27)].map(() => {
+        return {
+          student: faker.name.findName(),
+          score: Math.floor(Math.random() * 100),
+          question: faker.lorem.sentences(),
+          answer: faker.lorem.sentences(),
+          timestamp: faker.date.recent()
+        }
+      })
+    }
+  }
 }
 </script>
 
---
+++ src/components/professor/activeExam/SearchControl.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <div class="box">
+    <div class="field">
+      <p class="control has-icons-left">
+        <input
+          class="input"
+          type="text"
+          placeholder="Search history by student's name">
+        <span class="icon is-small is-left">
+          <i class="fa fa-search"></i>
+        </span>
+      </p>
+    </div>
+
+    <div class="field">
+      <p class="control has-icons-left">
+        <input
+          class="input"
+          type="text"
+          placeholder="Display questions with percent score higher than">
+        <span class="icon is-small is-left">
+          <i class="fa fa-percent"></i>
+        </span>
+      </p>
+    </div>
+
+    <div class="field">
+      <p class="control has-icons-left">
+        <input
+          class="input"
+          type="text"
+          placeholder="Display answers within last n minutes">
+        <span class="icon is-small is-left">
+          <i class="fa fa-hourglass-start"></i>
+        </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
